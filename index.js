36884f0d645baa8c0c93a4524e3a3f4abd8dd069
Rahulsainitech <rahuliim2001@gmail.com>
2023-07-19 21:04:00 +0530

    adding cors

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const port = process.env.PORT || 5000;
 const {keys} = require("./config/keys");
 const morgan= require("morgan");
 const mongoose = require("mongoose");
+const cors=require('cors');
 
 //middleware
+app.use(cors())
 app.use(express.json())
 app.use(morgan("dev"))
 
